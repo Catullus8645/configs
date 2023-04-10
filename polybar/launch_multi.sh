0442d7c0d2954f8d410c5e4f25ca765796679009
Catullus <49173557+Catullus8645@users.noreply.github.com>
2023-04-11 08:08:28 +1000

    Add files via upload

---
+++ polybar/launch_multi.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar, using default config location ~/.config/polybar/config
+#polybar monitor1 &
+#polybar monitor2 &
+#polybar mybar &
+#polybar mainbar-i3 &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload mybar &
+  done
+else
+  polybar --reload mybar &
+fi
+
+#polybar mybar &
+
+echo "Polybar launched..."
